379fae847c805526854babb69ea32d383b4dbe96
Lutopia <lutopia@hotmail.fr>
2025-03-08 22:01:28 +0100

    steve: Added /generate/custom endpoint

--- app/steve.py
+++ app/steve.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Request
+import json
 import subprocess
 import os
 import uuid
@@ -18,7 +19,25 @@ async def generate(configuration: str):
   config_path = f"{CFG_DIR}/{configuration}.steve.json"
   if not os.path.exists(config_path):
     raise HTTPException(status_code=404, detail=f"Configuration '{configuration}' not found")
+  return await generate_internal(config_path)
 
+@router.post("/generate/custom")
+async def generate_custom(request: Request):
+  """Generate a MIDI sequence using the custom configuration passed in the body of the request."""
+  try:
+    config = json.loads((await request.body()).decode())
+  except Exception:
+    raise HTTPException(status_code=415, detail=f"Request body couldn't be made into JSON configuration")
+  if "parents" in config and isinstance(config["parents"], list):
+    config["parents"] = list(map(lambda p: f"{CFG_DIR}/{p}.steve.json", config["parents"]))
+
+  config_path = f"tmp/{str(uuid.uuid4())}.steve.json"
+  with open(config_path, "w") as config_file:
+    json.dump(config, config_file)
+
+  return await generate_internal(config_path)
+
+async def generate_internal(config_path):
   filename = str(uuid.uuid4())
   output_path = f"tmp/{filename}"
   error = subprocess.call([
